e105fb1aa0958a466c70159d31e931ba794d335f
RODRIGODNS <rodrigodonsilva@gmail.com>
2022-06-11 12:03:07 -0300

    Code Refactor

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function btnClicked () {
-    const todoInput = document.getElementById('todoInput');
-    const list = document.getElementById('list');
+const todoInput = document.getElementById('todoInput');
 
+todoInput.addEventListener('keyup', onTextEnter);
+
+function onTextEnter (event) {
+    if (event.key != 'Enter' || event.keyCode != 13 || todoInput.value.trim() === "") {
+        return;
+    }
+
+    const list = document.getElementById('list');
     const inputValue = todoInput.value;
-    console.log(inputValue);
 
     const newTodo = document.createElement ('li');
+
+    newTodo.setAttribute('class','row');
+
     const newContent = document.createTextNode (inputValue);
 
     newTodo.appendChild(newContent);
